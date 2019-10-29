38ae9478db73b5ca0e62dfd8869ba8fa54a7c6e1
Felipe Perera <fpfelipeperera@gmail.com>
2019-10-29 16:11:24 -0300

    Modificaciones
    Modificaciones

--- P5/the_division/joy.js
+++ P5/the_division/joy.js
@@ -4,13 +4,34 @@ function setup() {
   slider = createSlider(0,100,0,10); //Slider, minimo,maximo,valor_inicial,step
   slider.style('width','200px');
   slider.parent('modifiers') //Mete el slider en el div con id 'modifiers'
+  noLoop();
+  stroke(0);
+  strokeWeight(2);
 }
 
 function draw() {
-	var scl=slider.value();
+	//var scl=slider.value();
+	background(255);
+	let scl=30;
+	for(let y=30;y<height;y+=30)
+	{
+	beginShape();
+			for(let x=0;x<=width+scl;x+=scl) 
+		{
+			if( x>width*0.3&&x<width*0.7)
+			{
+			curveVertex(x,y-random(10,75));
+			}else{
+				vertex(x,y);
+			}
+		}
+	endShape();
+	}
+	
 }
 
 function mouseClicked(){
+	clear();
 	draw();
 }
 
