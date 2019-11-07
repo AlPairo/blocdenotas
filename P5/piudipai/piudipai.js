41e00022353d62e41894fde878db409c1ff0a399
AlPairo <fpfelipeperera@gmail.com>
2019-11-06 22:55:20 -0300

    Diseño de remera piu
    Se crea diseño de remera y proyecto para crear diseños similares

---
+++ P5/piudipai/piudipai.js
@@ -0,0 +1,27 @@
+function setup() {
+  let base=createCanvas(650, 400); //Crea la base para el programa
+  base.parent('espacio');  //Mete el sketch en el div con id 'espacio'
+  /*slider = createSlider(0,100,0,10); //Slider, minimo,maximo,valor_inicial,step
+  slider.style('width','200px');
+  slider.parent('modifiers') //Mete el slider en el div con id 'modifiers'*/
+  boton = createButton('Generar');
+  boton.parent('modifiers');
+  boton.mouseClicked(redraw);
+  noLoop();
+  strokeWeight(30);
+  }
+
+function draw() {
+	var scl=30;
+	clear();
+	beginShape();
+	for (let x=0;x<width;x+=scl)
+	{
+		let rando=random(60,200);
+		curveVertex(x,rando);
+	
+		}
+		endShape();
+	//var scl=slider.value();
+}
+
