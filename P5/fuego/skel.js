7e40d7da16e63898e5fefea705680f7fed9d6f7b
FelipePerera <fpfelipeperera@gmail.com>
2019-11-26 08:55:43 -0300

    Modificaciones
    Se simplifica el codigo y se agregan sliders

--- P5/fuego/skel.js
+++ P5/fuego/skel.js
@@ -1,38 +1,53 @@
-
-
-
-
-
-
 function setup()
 {
-  let base=createCanvas(600, 400); //Crea la base para el programa
+  base=createCanvas(600, 400); //Crea la base para el programa
   base.parent('espacio');  //Mete el sketch en el div con id 'espacio'
-  background(0);
   stroke(255);
-  let slider1=createSlider(1,20,1,1);
-  let slider2=createSlider(1,30,1,5);
+  slider1=createSlider(1,height,1,1);
+  slider2=createSlider(0.01,2,1,0.03);
+  slider3=createSlider(1,150,75,2);
   slider1.style('width','200px');
   slider2.style('width','200px');
-  slider1.parent('modifiers');
-  slider2.parent('modifiers');
-}
+  slider3.style('width','200px');
+  slider1.parent('slider1');
+  slider2.parent('slider2');
+  slider3.parent('slider3');
+  }
+
 function draw(){
-//const val1=slider1.value();
-//const val2=slider2.value();
-translate(width/2,0);
+  background(0);
+  let val1=slider1.value();
+  let val2=slider2.value();
+  let val3=slider3.value();
+  translate(width/2,0);
+  senoX(val1,val2,val3);
+//  dibujo(val1,val2,val3);	
+
+} 
+function senoX(val1,val2,val3){
+	//val1 es la "amplitud de onda" => la escala de f(x)
+	//val3 es la "longitud de onda" => la escala de x
+	//val3 es el intervalo entre un punto y otro
+	noFill();
+	beginShape();
+	for (let y=0;y<height;y+=val2)
+	{
+	curveVertex(sin(y)*val1,y*val3);
+	}
+	endShape();
+} 
+function dibujo(val1,val2,val3){
 	for (let y=0;y<height;y+=0.1)
 	{
-		let a=sin(y)*100;
-		let b=y*30;
+		let a=sin(y)*val1;
+		let b=y*val2;
 		ellipse(a,b,2,2);
 		if((sin(y)!=0))
 		{
-			line(-width,b*25,a,b);
-			line(a,b,width,b*25);
+			line(-width,val3,a,b);
+			line(a,b,width,val3);
 		}
 	}
-}  
-
+}
 
 
--- P5/skel/skel.js
+++ P5/skel/skel.js
@@ -2,23 +2,16 @@ function setup() {
 
   let base=createCanvas(600, 400); //Crea la base para el programa
   base.parent('espacio');  //Mete el sketch en el div con id 'espacio'
-  slider = createSlider(0,100,0,10); //Slider, minimo,maximo,valor_inicial,step
+  slider = createSlider(0,255,0,10); //Slider, minimo,maximo,valor_inicial,step
   slider.style('width','200px');
   slider.parent('modifiers') //Mete el slider en el div con id 'modifiers'
 }
-let value=0;
 function draw() {
+
+let value=slider.value();
 	background(value);
 }
 
 
-
-function mouseClicked() {
-  if (value === 0) {
-    value = 255;
-  } else {
-    value = 0;
-  }
-}
 
 
