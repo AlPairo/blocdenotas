3f9d4481230e2599a9cb101cbcf4a6b963cfd082
Felipe Perera <fpfelipeperera@gmail.com>
2020-01-17 15:50:18 -0300

    qdsaa
    asdsa

--- P5/cuadraditos/cuadraditos.js
+++ P5/cuadraditos/cuadraditos.js
@@ -1,27 +1,42 @@
+var run=true;
 function setup() {
   var base=createCanvas(600, 400); //Crea la base para el programa
   base.parent('espacio');  //Mete el sketch en el div con id 'espacio'
   slider = createSlider(0,255,0,10); //Slider, minimo,maximo,valor_inicial,step
   slider.style('width','200px');
   slider.parent('modifiers') //Mete el slider en el div con id 'modifiers'
-  background(0);
-  fill(255);
-  noLoop();
-  var lista = [];
-  var cuadrado = {
-  	x:0,
-  	y:0,
-  	size:1
-  	toca: function(){
-  		return 
+  noStroke();
+ }
+
+function draw() {
+while(run==true){
+ var noise_zoom=0.001;
+ var chances = 5000;
+ var lista = [];
+ class cuadrado {
+  	constructor(a, b, c) {
+  		this.x = a;
+  		this.y = b;
+  		this.size = c;
+  		this.color= noise(this.x*noise_zoom,this.y*noise_zoom)*255;
   	}
-  };
-  for (var i = 0; i < 250; i++) {
-  	cuadrado.x=random(width);
-  	cuadrado.y=random(width);
-  	cuadrado.size=random(20);
-  	square(cuadrado.x,cuadrado.y,cuadrado.size);
-  }
+  	mostrar(){
+  		rotate(random()*PI);
+  		fill(this.color);
+  		circle(this.x,this.y,this.size);
+  	}}
+  for (var i = 0; i < 5000; i++) {
+  	lista.push(new cuadrado(random(-width,width),random(-height,height),random(30)));
+  	}
+
+  lista.forEach(element => element.mostrar());
+  run=false;  
+    }
+ }
+ 
+function mouseClicked() {
+clear();
+run=true;
 }
 
 
